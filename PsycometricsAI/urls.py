30557f23b6e90fcc774004bb71c5f0d8c0ec458d
Roberto-S-C <robertosanchezcazares@gmail.com>
2025-09-12 19:58:01 -0600

    Added Candidate Report Endpoint

--- PsycometricsAI/urls.py
+++ PsycometricsAI/urls.py
@@ -5,7 +5,7 @@ from PsycometricsAPI.views.microsoft_auth_view import microsoft_auth
 from PsycometricsAPI.views.google_auth_views import google_auth
 from PsycometricsAPI.views.hr_views import hr_candidates
 from PsycometricsAPI.views.candidate_views import verify_completed_test
-from PsycometricsAPI.views.report_views import create_report 
+from PsycometricsAPI.views.report_views import create_report, report_detail 
 
 
 urlpatterns = [
@@ -26,6 +26,7 @@ urlpatterns = [
     path("api/results/<str:id>/", result_views.result_detail),
 
     path('api/reports/', create_report),
+    path("api/reports/candidates/<str:id>/", report_detail),
 
     path('api/signup/', email_auth_views.signup),
     path('api/login/', email_auth_views.login),
--- PsycometricsAPI/views/report_views.py
+++ PsycometricsAPI/views/report_views.py
@@ -31,3 +31,17 @@ def create_report(request):
             return Response({"report_id": str(result.inserted_id)}, status=201)
 
         return Response(serializer.errors, status=400)
+
+@api_view(["GET"])
+def report_detail(request, id):
+    try:
+        candidate_id = ObjectId(id)
+    except Exception:
+        return Response({"error": "Invalid candidate ID"}, status=400)
+
+    report = report_collection.find_one({"candidate_id": candidate_id})
+    if not report:
+        return Response({"error": "Report not found"}, status=404)
+
+    report = convert_objectid(report)
+    return Response(report, status=200)
